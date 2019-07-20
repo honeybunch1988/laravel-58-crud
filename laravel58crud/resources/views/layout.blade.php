06a307d9ea956e4ab065601c62835ee6d0f2676f
honeybunch1988 <47169517+honeybunch1988@users.noreply.github.com>
2019-07-20 08:33:54 +0800

    Jul 20 - update layout.blade.php
    design didn't take effect..
    syntax error in the spacing..

--- laravel58crud/resources/views/layout.blade.php
+++ laravel58crud/resources/views/layout.blade.php
@@ -14,7 +14,7 @@
         @yield('content')
     </div>
 
-    <script src = "{ {asset('js/app.js')} }" type = "text/js"></script>
+    <script src = "{{asset('js/app.js')}}" type = "text/js"></script>
 
 </body>
 </html>
